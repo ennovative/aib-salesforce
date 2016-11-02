4553a04b91ba631bee45c992a869ca2a04604b19
Jon Waples <jon@ennovative.com.au>
2016-11-02 13:18:31 +1030

    Set search result dropdown bar to hidden

--- dist/aib-salesforce.js
+++ dist/aib-salesforce.js
@@ -1,4 +1,4 @@
-/*! aib-salesforce - v1.0.0 - 2016-10-28
+/*! aib-salesforce - v1.0.0 - 2016-11-02
 * https://github.com/ennovative/aib-salesforce#readme
 * Copyright (c) 2016 ; Licensed  */
 function toggleNavigation() {
