1159374ff0ca9ea9710cbad114ca55fbf958ed60
Jon Waples <jon@ennovative.com.au>
2016-11-21 11:21:48 +1030

    Footer bg cover and profile hover colour change

--- dist/aib-salesforce.js
+++ dist/aib-salesforce.js
@@ -1,4 +1,4 @@
-/*! aib-salesforce - v1.0.0 - 2016-11-18
+/*! aib-salesforce - v1.0.0 - 2016-11-21
 * https://github.com/ennovative/aib-salesforce#readme
 * Copyright (c) 2016 ; Licensed  */
 function toggleNavigation() {
